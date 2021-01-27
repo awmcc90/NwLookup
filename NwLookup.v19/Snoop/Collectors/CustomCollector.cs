60b51c5cd47a97df27345ba83f4186e305a75908
Piotr Kulicki <piotr.kulicki@pmgroup-global.com>
2021-01-27 11:16:07 +0000

    Added Manage 2020 & 2019 Versions

---
+++ NwLookup.v19/Snoop/Collectors/CustomCollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using NwLookup.Snoop.Collectors;
+
+namespace NwLookup.v19.Snoop.Collectors
+{
+    public class CustomCollector : Collector
+    {
+        public CustomCollector()
+            : base(new List<IStreamPass>{
+                new DefaultStreamPass(),
+                new ModelItemStreamPass()
+            })
+        { }
+    }
+}
---
+++ NwLookup.v20/Snoop/Collectors/CustomCollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using NwLookup.Snoop.Collectors;
+
+namespace NwLookup.v20.Snoop.Collectors
+{
+    public class CustomCollector : Collector
+    {
+        public CustomCollector()
+            : base(new List<IStreamPass>{
+                new DefaultStreamPass(),
+                new ModelItemStreamPass()
+            })
+        { }
+    }
+}

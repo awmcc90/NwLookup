66a450acc1e544ea748da3a6e47e67e5b32b6452
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-02-18 09:40:24 +0700

    Support Navis 2022

---
+++ NwLookup.v22/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NwLookup.v22")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AMCC")]
+[assembly: AssemblyProduct("NwLookup.v22")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("AF42C1C1-044A-421A-A39F-4A6115B6572E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2022.0.0.0")]
+[assembly: AssemblyFileVersion("2022.0.0.0")]
---
+++ NwLookup.v22/Snoop/Collectors/CustomCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NwLookup.Snoop.Collectors;
+
+namespace NwLookup.v22.Snoop.Collectors
+{
+    public class CustomCollector : Collector
+    {
+        public CustomCollector() 
+            : base(new List<IStreamPass>{
+                new DefaultStreamPass(),
+                new ModelItemStreamPass()
+            }) { }
+    }
+}
